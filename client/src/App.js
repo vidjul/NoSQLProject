9491f49419f92860f655a6765ddb7e92aaf1969f
Sajeevan Puvikaran <Sajeevan96@hotmail.fr>
2018-03-16 15:44:46 +0100

    initial view

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import InputComponent from './Components/InputComponent';
 import DatepickerComponent from './Components/DatepickerComponent';
-import { Container } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -16,8 +16,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Container>
-          <DatepickerComponent /><br />
+        <Container className="text-center">
+          <Row>&emsp;Date :&emsp; <DatepickerComponent /></Row><br />
           <InputComponent fieldType="Exchanges" /><br />
           <InputComponent fieldType="Orgs" /><br />
           <InputComponent fieldType="People" /><br />
